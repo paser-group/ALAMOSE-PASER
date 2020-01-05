0e2af8c8abcd2995d1283101353cf5d303f7fe5d
Akond Rahman <akond.rahman.buet@gmail.com>
2020-01-05 17:44:55 -0600

    get files

--- csc6220_miner.py
+++ csc6220_miner.py
@@ -32,8 +32,22 @@ def getEligibleProjects(fileNameParam):
   return repo_list
 
 def getModFilesInDiff(diff_str):
+    mod_files  = []
     splitted_lines = diff_str.split('\n')
-    mod_files = [x_.split('a')[-1] for x_ in splitted_lines if '---' in x_ ]
+    for x_ in splitted_lines:
+      if '/' in x_  and '.' in x_: 
+        if '---' in x_:
+          # print(splitted_lines) 
+          del_ =  x_.split(' ')[-1].replace('a', '')
+          mod_files.append(del_) 
+        elif '+++' in x_:
+          # print(splitted_lines) 
+          add_ =  x_.split(' ')[-1].replace('b', '')
+          mod_files.append(add_)
+    mod_files = np.unique(mod_files) 
+    mod_files = [x_ for x_ in mod_files if ('doc' not in x_) and ('test' not in x_) ]
+    # print(mod_files)
+    # print(';'*25)
     return mod_files
 
 def getDiff(repo_, hash_):
@@ -45,21 +59,52 @@ def getDiff(repo_, hash_):
     command2Run = cdCommand + diffCommand
     try:
       diff_output = subprocess.check_output(["bash", "-c", command2Run])
+      diff_output =  diff_output.decode('latin-1') ### exception for utf-8 
+      # print(diff_output) 
       mod_files_list =  getModFilesInDiff(diff_output)
     except subprocess.CalledProcessError as e_:
       diff_output = "NOT_FOUND" 
     return diff_output, mod_files_list
 
+def filterFiles(ls_):
+  mod_list = [] 
+  for elem in ls_:
+    z_  = elem.split('.')[-1].lower() 
+    # print(z_) 
+    if ( ('md' not in z_)   and ('rst' not in z_) and  \
+                                  ('html' not in z_) and ('txt' not in z_) and  \
+                                  ('json' not in z_) and ('test' not in z_) and  \
+                                  ('html' not in z_) and ('image' not in z_) and  \
+                                  ('git' not in z_) and  ('dat' not in z_)  and ('travis' not in z_) and  ('inl' not in z_) and \
+                                  ('mod' not in z_) and  ('cfg' not in z_)  and ('json' not in z_) and  ('xml' not in z_)   ): 
+                                  mod_list.append( elem  ) 
+  return mod_list 
 
-def buildContent(df_, HOST_DIR):
-    all_hash = np.unique( df_['HASH'].tolist() ) 
+def buildContent(df_, HOST_DIR, repo_type):
+    content = [] 
+    repo_df  = df_[df_['REPO_TYPE']==repo_type]
+    all_repo_cnt = len( np.unique(repo_df['REPO'].tolist()) )  
+    print('TOTAL REPOS ARE {} FOR {}'.format(  all_repo_cnt, repo_type) ) 
+    all_hash = np.unique( repo_df['HASH'].tolist() ) 
     for hash_ in all_hash: 
         hash_df = df_[df_['HASH']==hash_]
         repo_name = hash_df['REPO'].tolist()[0]
         secu_flag = hash_df['SECU_FLAG'].tolist()[0] 
         repo_path = HOST_DIR + repo_name + '/'
         diff_txt, file_list  = getDiff(repo_path, hash_) 
-        print(file_list) 
+        repo_type = hash_df['REPO_TYPE'].tolist()[0] 
+        filtered_files = filterFiles(file_list) 
+        # print(repo_path, filtered_files) 
+        # print('<>'*25)
+        for fil_ in filtered_files: 
+          file_path     = HOST_DIR + repo_name + fil_ 
+          if(os.path.exists(file_path)):
+            map_name      = file_path.replace('/', '_')
+            tuple_        = (repo_name, repo_path, repo_type, hash_, file_path, map_name, secu_flag)  
+            print(tuple_) 
+            content.append( tuple_ )
+
+
 
 if __name__=='__main__': 
 
@@ -70,8 +115,9 @@ if __name__=='__main__':
     CURATED_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Teaching/Spring2020/CSC6220-TNTECH/CURATED_SECURITY_FULL.csv' 
     HOST_DIR='/Users/arahman/TEACHING_REPOS/NON_JULIA_SCIENTIFIC_SOFTWARE/'
 
-    CURATED_DF = pd.read_csv(CURATED_FILE) 
-    buildContent(CURATED_DF)
+    CURATED_DF   = pd.read_csv(CURATED_FILE) 
+    TYPE2ANALYZE = 'ComputationalBiology'
+    buildContent(CURATED_DF, HOST_DIR, TYPE2ANALYZE) 
 
     print('*'*100 )
     print('Ended at:', giveTimeStamp() )
